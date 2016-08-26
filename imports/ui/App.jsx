6f63cf486400d133ed8d73b017f9e5618e8f8c23
yanjie <yanjie.pan@brightcells>
2016-08-26 11:50:12 +0800

    save

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,16 +1,39 @@
 import React, {Component} from 'react'
 import NavBar from './shared/NavBar.jsx'
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import Radium, { StyleRoot } from 'radium'
+import AppBar from 'material-ui/AppBar'
+
 class App extends Component {
+
+  componentWillMount () {
+    let setNavBarState = () => this.setState({renderNavBar: window.innerWidth > 700})
+    setNavBarState()
+    window.onresize = setNavBarState
+  }
+
   getChildContext () {
     return { muiTheme: getMuiTheme() }
   }
+
+  getStyles() {
+    return {
+      root: {
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+      }
+    }
+  }
   render () {
+    const styles = this.getStyles()
     return (
-      <div>
-        <NavBar />
-        {this.props.children}
-      </div>
+      <StyleRoot>
+        <div style = {styles.root}>
+          {this.state.renderNavBar ? <NavBar /> : <AppBar />}
+          {this.props.children}
+        </div>
+      </StyleRoot>
     )
   }
 }
@@ -19,4 +42,4 @@ App.childContextTypes = {
   muiTheme: React.PropTypes.object.isRequired
 }
 
-export default App
+export default Radium(App)
