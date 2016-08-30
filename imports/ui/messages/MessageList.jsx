6a2d9bbcf41b4cb80e2cc59e01f0df6d746c9211
jackpanyj <1016432619@qq.com>
2016-08-30 15:15:31 +0800

    done

--- imports/ui/messages/MessageList.jsx
+++ imports/ui/messages/MessageList.jsx
@@ -2,12 +2,36 @@ import React, { Component } from 'react';
 import map from 'lodash/fp/map';
 import Radium from 'radium';
 import Message from './Message.jsx';
+import CircularProgress from 'material-ui/CircularProgress';
 class MessageList extends Component {
+  componentDidMount() {
+    $(".loader").delay(800).fadeOut('slow', function() {
+      $("#messages").fadeIn('slow');
+      let msgList = document.getElementById('message-list');
+      if(msgList !== null) {
+        msgList.scrollTop = msgList.scrollHeight;
+      }
+    });
+  }
+  componentDidUpdate() {
+    let msgList = document.getElementById('message-list');
+    if(msgList !== null) {
+      msgList.scrollTop = msgList.scrollHeight;
+    }
+  }
   render() {
     let styles = {
+      circle: {
+        margin: '0 auto',
+        paddingTop: '100px',
+        display: 'block'
+      },
       list: {
         flexGrow: 1,
         overflowY: 'auto',
+      },
+      messages: {
+        display: 'none'
       }
     };
     const allMessages = map((message) => {
@@ -16,10 +40,14 @@ class MessageList extends Component {
       );
     }, this.props.messages);
     return (
-      <div style={styles.list}>
-        <div>
-          { allMessages }
-        </div>
+      <div id="message-list" style={styles.list}>
+       <CircularProgress
+         mode="indeterminate"
+         className="loader"
+         style={styles.circle} />
+       <div id="messages" style={styles.messages}>
+         { allMessages }
+       </div>
       </div>
     );
   }
